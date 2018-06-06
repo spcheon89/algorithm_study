3fbffccb108633ae35c5706b678681c1cbf6ff6d
spcheon89 <spcheon89@gmail.com>
2018-06-06 15:32:46 +0900

    solved 2025 problem

---
+++ sw_expert_problem/D1/2025.cpp
@@ -0,0 +1,25 @@
+/*
+1부터 주어진 숫자만큼 모두 더한 값을 출력하는 프로그램 작성
+
+[input]
+자연수 1개 (10000이하)
+
+[output]
+1부터 입력에 주어진 수까지의 자연수 합
+*/
+
+#include <iostream>
+using namespace std;
+
+int input;
+int sum = 0;
+
+int main()
+{
+    cin >> input;
+    for(int i=1; i<=input; i++)
+    {
+        sum += i;
+    }
+    return 0;
+}
